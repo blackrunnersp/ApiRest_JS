8ab2b8d29bd0690b4647d7502443dba5c7a94c17
blackrunnersp <blackrunnersp@hotmail.it>
2024-07-20 17:53:13 +0200

    sixthy Commit

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ app.use('/api/users', require('./api/users'));
 app.use('/api/metadata', require('./api/metadata'));
 app.use('/api/userWatched', require('./api/userWatched'));
 
-// Middleware per gestire gli endpoint non trovati
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Endpoint not found.' });
 });
 
-// Esporta l'app come una funzione serverless per Vercel
+// Exporta come una funzione serverless per Vercel
 module.exports.handler = serverless(app);
