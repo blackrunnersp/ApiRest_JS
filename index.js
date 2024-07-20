9b3edc9efb2f5c0d6fdc64c3d9e73c9f3bec7e17
blackrunnersp <blackrunnersp@hotmail.it>
2024-07-20 20:13:09 +0200

    Riparato

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import serverless from 'serverless-http';
 import usersRouter from './api/users.js';
 import metadataRouter from './api/metadata.js';
 import userWatchedRouter from './api/userWatched.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -11,8 +14,9 @@ app.use('/api/users', usersRouter);
 app.use('/api/metadata', metadataRouter);
 app.use('/api/userWatched', userWatchedRouter);
 
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(404).json({ message: 'Endpoint not found.' });
 });
 
+// Esporta come handler per Vercel
 export const handler = serverless(app);
