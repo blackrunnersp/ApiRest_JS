e3b556c0848393b353f42e1d66b5651bd2832d31
blackrunnersp <blackrunnersp@hotmail.it>
2024-07-20 17:38:53 +0200

    five Commit

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
 const pool = require('./config/database');
 require('dotenv').config();
 
 app.use(express.json());
 
-app.use('/api/user', require('./api/user'));
-app.use('/api/userwatched', require('./api/userWatched'));
+app.use('/api/users', require('./api/users'));
 app.use('/api/metadata', require('./api/metadata'));
+app.use('/api/userWatched', require('./api/userWatched'));
 
+// Middleware per gestire gli endpoint non trovati
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Endpoint not found.' });
 });
 
-// Per Vercel, devi esportare l'app come una funzione
-module.exports = app;
+// Esporta l'app come una funzione serverless per Vercel
+module.exports.handler = serverless(app);
