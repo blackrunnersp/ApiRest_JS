0f78e576d2f03ffc2ad5674635341bafdeb0de0f
blackrunnersp <blackrunnersp@hotmail.it>
2024-07-21 01:52:55 +0200

    Prima release funzionante

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const { createServer } = require('http');
+const { parse } = require('url');
 const app = express();
 const port = 3000;
-const serverless = require('serverless-http');
-const pool = require('./config/database'); // Configurazione del database
-const authenticate = require('./utils/authenticate');
 
 // Middleware per parsing del corpo delle richieste in JSON
 app.use(express.json());
@@ -19,7 +18,6 @@ app.post('/api/data', (req, res) => {
 });
 
 // Rotte API
-
 app.use('/api/users', require('./api/users'));
 app.use('/api/test', require('./api/test'));
 app.use('/api/metadata', require('./api/metadata'));
@@ -30,7 +28,11 @@ app.use((req, res, next) => {
   res.status(404).json({ message: 'Endpoint non trovato.' });
 });
 
+// Funzione handler per Vercel
+module.exports = (req, res) => {
+  const server = createServer(app);
+  const parsedUrl = parse(req.url, true);
 
-
-// **Corrected Export:**
-module.exports.handler = serverless(app);
+  // Simula il comportamento di Express con il server HTTP
+  server.emit('request', req, res);
+};
