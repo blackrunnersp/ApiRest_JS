98136e8c8d1c205aadc551631a9a6330f2ff6bb8
blackrunnersp <blackrunnersp@hotmail.it>
2024-08-03 18:54:34 +0200

    Aggiunto script per auth cookie

--- api/authcookie.js
+++ api/authcookie.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const dbClient = require('../config/database');
-const authenticate = require('../utils/authenticate');
-const app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const bodyParser = require('body-parser');
 
-app.get('/', function(req, res) {
+// Middleware per parsing del corpo delle richieste in JSON e URL encoded
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+// Definisci le rotte usando `router`
+router.get('/', (req, res) => {
   res.send("ok.");
 });
 
-app.post('/', function(req, res) {
-  var jscode = req.body.jscode;
+router.post('/', (req, res) => {
+  let jscode = req.body.jscode;
+  
+  // Modifiche al codice JavaScript
   jscode = jscode.replace("/aes.js", "https://pastebin.com/raw/pKrFHFzf");
   jscode = jscode.replace("location.href", "var uselessvar12345");
-  jscode = new JSDOM(jscode, { runScripts: "dangerously", resources: "usable" });
-  setTimeout(function() {
-  res.send(jscode.window.document.cookie)}, 500);
-  return;
+
+  // Utilizzo di JSDOM
+  const dom = new JSDOM(jscode, { runScripts: "dangerously", resources: "usable" });
+  
+  // Risposta con il cookie dopo un timeout
+  setTimeout(() => {
+    res.send(dom.window.document.cookie);
+  }, 500);
 });
 
-
+// Esporta il router per essere utilizzato in index.js
 module.exports = router;
