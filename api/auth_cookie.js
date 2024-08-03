827fb14b87870ba478a047418eed7ef3110ba80a
blackrunnersp <blackrunnersp@hotmail.it>
2024-08-03 18:44:09 +0200

    Aggiunto script per auth cookie

---
+++ api/auth_cookie.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const dbClient = require('../config/database');
+const authenticate = require('../utils/authenticate');
+const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+app.get('/', function(req, res) {
+  res.send("ok.");
+});
+
+app.post('/', function(req, res) {
+  var jscode = req.body.jscode;
+  jscode = jscode.replace("/aes.js", "https://pastebin.com/raw/pKrFHFzf");
+  jscode = jscode.replace("location.href", "var uselessvar12345");
+  jscode = new JSDOM(jscode, { runScripts: "dangerously", resources: "usable" });
+  setTimeout(function() {
+  res.send(jscode.window.document.cookie)}, 500);
+  return;
+});
+
+
+module.exports = router;
